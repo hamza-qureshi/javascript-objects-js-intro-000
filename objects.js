291330158c402f5b2ccf82953f6048a8c727e15e
Hamza Qureshi <hamza.qureshi97@gmail.com>
2019-08-01 15:52:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,11 +2,11 @@ var playlist = { 'coldplay' : 'Everglow',
             'snow patrol' : 'NewYork'}
 
 function updatePlaylist (playlist,artist,title) {
-  playlist.artist = title
+  playlist[artist] = title
   return playlist
 }
 
 function removeFromPlaylist (playlist,artist){
-  delete playlist.artist
+  delete playlist[artist]
   return playlist
 }
